294644ba37346527154e0ea4623d5fcf16a1bee7
Marcos Donaciano Teixeira Júnior <marcosdjunior@gmail.com>
2023-03-01 17:18:40 -0300

    Configuração da conexão com o banco atualizada

--- src/connection.ts
+++ src/connection.ts
@@ -1,9 +1,11 @@
 import './env';
 import { Sequelize } from 'sequelize';
+import pg from 'pg';
 
 const sequelize = new Sequelize(process.env.DB_NAME ?? '', process.env.DB_USER ?? '', process.env.DB_PASSWORD ?? '', {
     host: process.env.DB_HOST ?? '',
     dialect: 'postgres',
+    dialectModule: pg,
     port: 5432
 });
 
