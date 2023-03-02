b1261f45d7b96454e589b72d800294c705e00ae0
Marcos Donaciano Teixeira Júnior <marcosdjunior@gmail.com>
2023-03-02 16:05:25 -0300

    Alteração de mensagem de validação de nome de usuário

--- src/utils/validations/user.ts
+++ src/utils/validations/user.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 const data = z.object({
     username: z.string()
-        .min(3, { message: 'O campo username necessita de no mínimo 3 caracteres' }),
+        .min(3, { message: 'O nome de usuário necessita ter no mínimo 3 caracteres' }),
     password: z.string()
         .regex(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/,
             { message: 'A senha precisa ter no mínimo 8 caracteres com pelo menos uma letra maiúscula e um número' })
